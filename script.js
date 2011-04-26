81fdab719737024172ff1096b73eb298ddfb765d
Tsachi Shlidor <tsi@322980.no-reply.drupal.org>
2011-04-26 21:20:39 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+(function ($) {
+
+  Drupal.behaviors.hideTips = {
+    attach: function(context) {
+      
+      /* 
+       *  Hide and show formatting tips.
+       */
+      var tips = $(".filter-wrapper .fieldset-wrapper > *");
+      // Hide formating tips by default
+      $(tips).not(".filter-help").hide();
+      // Show Tips On Click and unbind for the next click
+      $("a", tips).click(function() {
+        $(this).parents(".filter-help").siblings().show();
+        $(this).unbind('click');
+        return false;
+      });
+            
+    }
+
+  };
+
+})(jQuery);
