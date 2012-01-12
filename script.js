0f6dc89aab4bcb61d29caa6fe2fb8b29db104fd4
Tsachi Shlidor <tsi.shlidor@gmail.com>
2012-01-12 13:12:06 +0200

    Leave the drop-down visible

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
        */
       var tips = $(".filter-wrapper .fieldset-wrapper > *");
       // Hide formating tips by default
-      $(tips).not(".filter-help").hide();
+      $(tips).not(".filter-help, .form-type-select").hide();
       // Show Tips On Click and unbind for the next click
       $("a", tips).click(function() {
         $(this).parents(".filter-help").siblings().show();
