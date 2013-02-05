b9898363e9cd08652d3fe1baf6902f86739ad14b
Tsachi Shlidor <tsi.shlidor@gmail.com>
2013-02-05 13:35:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,18 @@
 
   Drupal.behaviors.hideTips = {
     attach: function(context) {
-      
-      /* 
-       *  Hide and show formatting tips.
-       */
+
+      //  Hide and show formatting tips.
       var tips = $(".filter-wrapper .fieldset-wrapper > *");
       // Hide formating tips by default
       $(tips).not(".filter-help, .form-type-select").hide();
       // Show Tips On Click and unbind for the next click
-      $("a", tips).click(function() {
+      $("a", tips).click(function(e) {
+        e.preventDefault();
         $(this).parents(".filter-help").siblings().show();
         $(this).unbind('click');
-        return false;
       });
-            
+
     }
 
   };
